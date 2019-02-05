e558ee3010be76ad79ea4e0be21603c57ef4685f
Mike Flores <mike.flores3717@gmail.com>
2019-02-05 17:33:41 -0600

    test commit

---
+++ src/main/java/com/example/movieapplication/MovieapplicationApplication.java
@@ -0,0 +1,14 @@
+package com.example.movieapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieapplicationApplication.class, args);
+    }
+
+}
+

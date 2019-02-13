434fe68fa95e036d9652e064eae64812e69d489a
Mike Flores <mike.flores3717@gmail.com>
2019-02-13 16:55:21 -0600

    Adding backend stuff

---
+++ src/main/java/com/example/movieapplication/model/User.java
@@ -0,0 +1,70 @@
+package com.example.movieapplication.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/movieapplication/repository/Users.java
@@ -0,0 +1,8 @@
+package com.example.movieapplication.repository;
+
+import com.example.movieapplication.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface Users extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
